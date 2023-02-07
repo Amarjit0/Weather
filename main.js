9ecb3f6876948125f8d61f42cff9f851f9d476f1
verma888 <sevtagourav@gmail.com>
2023-02-07 17:15:26 +0530

    gggggggggggggggggggggg

--- main.js
+++ main.js
@@ -1,36 +1,49 @@
+const input = document.querySelector('#cityinput');
+const btn = document.querySelector('#add');
+const city = document.querySelector('#cityoutput');
+const description = document.querySelector('#description');
+const temperature = document.querySelector('#temp');
+const wind = document.querySelector('#wind');
+const dateTime = document.querySelector('#datetime');
 
-//selector variable
-var inputval = document.querySelector('#cityinput')
-var btn = document.querySelector('#add');
-var city = document.querySelector('#cityoutput')
-var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
 
 
-// Get your own free OWM API key at https://www.openweathermap.org/appid - please do not re-use mine!
-// You don't need an API key for this to work at the moment, but this will change eventually.
-apik = "3045dd712ffe6e702e3245525ac7fa38"
-//kelvin to celcius
-function convertion(val){
-    return (val - 273).toFixed(2)
-}
-//fetch
-    btn.addEventListener('click', function(){
-        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
-        .then(res => res.json())
-         //.then(data => console.log(data))
-        .then(data => {
-            var nameval = data['name']
-            var descrip = data['weather']['0']['description']
-            var tempature = data['main']['temp']
-            var wndspd = data['wind']['speed']
+const API_KEY = "3045dd712ffe6e702e3245525ac7fa38";
 
-            city.innerHTML=`City: ${nameval}`
-            temp.innerHTML = `Temperature: ${ convertion(tempature)} C`
-            description.innerHTML = `Conditions: ${descrip}`
-            wind.innerHTML = `Wind Speed: ${wndspd} km/h`
+const kelvinToCelsius = (kelvin) => (kelvin - 273.15).toFixed(2);
 
-        })
-        .catch(err => alert('You entered Wrong city name'))
-    }) 
+const formatDate = (date) =>
+  `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+  
+  
+fetch('./cities.json')
+.then((response) => response.json())
+.then((data) => {
+  const cities = data;
+  const datalist = document.querySelector('#cities');
+  for (const city of cities) {
+    const option = document.createElement('option');
+    option.value = city;
+    datalist.appendChild(option);
+  }
+});
+
+
+
+btn.addEventListener('click', () => {
+  
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerHTML = `City: ${data.name}`;
+      temperature.innerHTML = `Temperature: ${kelvinToCelsius(data.main.temp)} C`;
+      description.innerHTML = `Conditions: ${data.weather[0].description}`;
+      wind.innerHTML = `Wind Speed: ${data.wind.speed} km/h`;
+      dateTime.innerHTML = `Date and Time: ${formatDate(new Date())}`;
+    })
+    .catch(() => alert('You entered an incorrect city name'));
+});
+
+
+
